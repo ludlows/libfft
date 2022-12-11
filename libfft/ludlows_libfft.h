ee7f34adbcc8fdda647a476fa58b6bd5fef3606a
ludlows <ludlows@users.noreply.github.com>
2022-12-11 22:55:18 +0800

    update c code

--- libfft/ludlows_libfft.h
+++ libfft/ludlows_libfft.h
@@ -22,11 +22,13 @@ struct fft_ctx_double {
     double * x;                    // n_fft << 1, real and image parts
 };
 
+typedef struct fft_ctx_double* fft_ctx_double_ptr;
+
 // init fft_ctx_double
-struct fft_ctx_double* init_fft_ctx_double(unsigned long n); //
+fft_ctx_double_ptr init_fft_ctx_double(unsigned long n); //
 
 // free fft_ctx_double
-void free_fft_ctx_double(struct fft_ctx_double* pointer); // 
+void free_fft_ctx_double(fft_ctx_double_ptr pointer); //
 
 // n >= 1
 int is_power_of_2(unsigned long n);
@@ -35,10 +37,10 @@ int is_power_of_2(unsigned long n);
 unsigned long next_power_of_2(unsigned long x);
 
 // fft
-void fft_double_inplace(struct fft_ctx_double* ctx);
+void fft_double_inplace(fft_ctx_double_ptr ctx_ptr);
 
 // ifft
-void ifft_double_inplace(struct fft_ctx_double* ctx);
+void ifft_double_inplace(fft_ctx_double_ptr ctx_ptr);
 
 int is_power_of_2(unsigned long n) { // n>=1
     return ((n & (n - 1)) == 0);
@@ -52,13 +54,13 @@ unsigned long next_power_of_2(unsigned long n) {
     return k;
 }
 
-struct fft_ctx_double* init_fft_ctx_double(unsigned long n) {
+fft_ctx_double_ptr init_fft_ctx_double(unsigned long n) {
     void * p = malloc(sizeof(struct fft_ctx_double));
     if (p == NULL) {
         printf("allocate memory of struct fft_ctx_double failed!\n");
         return NULL;
     }
-    struct fft_ctx_double* p_ctx = (struct fft_ctx_double*)p;
+    fft_ctx_double_ptr p_ctx = (fft_ctx_double_ptr)p;
     p_ctx->n_fft = n;
     p_ctx->buffer_info = NULL;
     p_ctx->cos_sin_info = NULL;
@@ -99,7 +101,7 @@ struct fft_ctx_double* init_fft_ctx_double(unsigned long n) {
     return p_ctx;
 }
 
-void free_fft_ctx_double(struct fft_ctx_double* pointer) {
+void free_fft_ctx_double(fft_ctx_double_ptr pointer) {
     if (pointer == NULL) return;
     free(pointer->bit_swap);
     pointer->bit_swap = NULL;
@@ -120,20 +122,20 @@ x: length 2 * n_point, signal x(n) whose length is n_point only
 x[0]: signal x(0)'s real part
 x[1]: signal x(0)'s image part
 */
-void fft_double_inplace(struct fft_ctx_double* ctx) {
-    if (ctx == NULL) {
+void fft_double_inplace(fft_ctx_double_ptr ctx_ptr) {
+    if (ctx_ptr == NULL) {
         printf("Error! fft context is NULL.\n");
         return;
     }
-    unsigned long n_point = ctx->n_fft;
+    unsigned long n_point = ctx_ptr->n_fft;
     if (n_point <= 1) {
         printf("Error! nFFT <= 1.\n");
         return;
     }
-    double * x = ctx->x;
-    unsigned long * buffer_info = ctx->buffer_info;
-    double * cos_sin_info = ctx->cos_sin_info;
-    unsigned long * bit_swap = ctx->bit_swap;
+    double * x = ctx_ptr->x;
+    unsigned long * buffer_info = ctx_ptr->buffer_info;
+    double * cos_sin_info = ctx_ptr->cos_sin_info;
+    unsigned long * bit_swap = ctx_ptr->bit_swap;
     register double theta, index1_real, index2_real, index1_image, index2_image;
     unsigned long k = 0;
     // initialize cos_sin_info
@@ -210,17 +212,20 @@ x: length 2 * n_point, spectrum X(n) whose length is n_point only
 x[0]: spectrum X(0)'s real part
 x[1]: spectrum X(0)'s image part
 */
-void ifft_double_inplace(struct fft_ctx_double* ctx) {
-    if (ctx == NULL) {
+void ifft_double_inplace(fft_ctx_double_ptr ctx_ptr) {
+    if (ctx_ptr == NULL) {
         printf("Error! fft context is NULL.\n");
         return;
     }
-    unsigned long n_point = ctx->n_fft;
-    if (n_point <= 1) return;
-    double * x = ctx->x;
-    unsigned long * buffer_info = ctx->buffer_info;
-    double * cos_sin_info = ctx->cos_sin_info;
-    unsigned long * bit_swap = ctx->bit_swap;
+    unsigned long n_point = ctx_ptr->n_fft;
+    if (n_point <= 1) {
+        printf("Error! nFFT <= 1\n");
+        return;
+    }
+    double * x = ctx_ptr->x;
+    unsigned long * buffer_info = ctx_ptr->buffer_info;
+    double * cos_sin_info = ctx_ptr->cos_sin_info;
+    unsigned long * bit_swap = ctx_ptr->bit_swap;
     register double theta, index1_real, index2_real, index1_image, index2_image;
     unsigned long k = 0;
     // initialize cos_sin_info
